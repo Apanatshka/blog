1d2bc14bf0da81fdfb7b0df98d3eb7627681f17c
Jeff Smits <jeff.smits@gmail.com>
2024-06-08 17:59:22 +0200

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-zola build
-git checkout master
-mv public/* .
-git add .
-git commit -m "Site updated at $(date -u --rfc-3339=seconds)"
-git push origin master
+zola build && \
+git checkout master && \
+cp -r public/* . && \
+git add . && \
+git commit -m "Site updated at $(date -u --rfc-3339=seconds)" #&& \
+git push origin master && \
 git checkout zola
